f8063405bf65dc2a41da9594c1d8c2d68097db18
RG-Im <94027045+RK-IM@users.noreply.github.com>
2023-06-17 23:01:53 +0900

    Update train.py

--- Kaggle/HuBMAP_Hacking_the_Kidney/code/train.py
+++ Kaggle/HuBMAP_Hacking_the_Kidney/code/train.py
@@ -1,12 +1,15 @@
+import gc
 import torch
 from tqdm.auto import tqdm
 
-from utils import dice
+from utils import dice, save_log
 
+from IPython.display import clear_output
 
 class Trainer():
     def __init__(self,
                  config,
+                 fold,
                  train_loader,
                  valid_loader,
                  model,
@@ -18,10 +21,11 @@ class Trainer():
         self.early_stop = config.early_stop
         self.patience = config.early_stop_patience
         self.log_dir = config.log_dir
-        self.fold = config.fold
+        self.nb_splits = config.nb_splits
         self.device = config.device
         self.use_amp = config.use_amp
 
+        self.fold = fold
         self.train_loader = train_loader
         self.valid_loader = valid_loader
         self.model = model
@@ -32,7 +36,7 @@ class Trainer():
         self.scaler = torch.cuda.amp.GradScaler(enabled=self.use_amp)
 
 
-    def iteration(self, phase):
+    def iteration(self, epoch, phase):
         if phase == 'train':
             dataloader = self.train_loader
             self.model.train()
@@ -44,9 +48,9 @@ class Trainer():
         iter_loss = 0
         iter_dice = 0
 
-        tk0 = tqdm(dataloader, total=batches)
-        tk0.set_description(desc=f"{phase}")
-        for i, (img, msk) in enumerate(tk0, 1):
+        tk1 = tqdm(dataloader, total=batches)
+        tk1.set_description(desc=f"{phase}")
+        for i, (img, msk) in enumerate(tk1, 1):
             img, msk = img.to(self.device), msk.to(self.device)
 
             if phase == 'train':
@@ -68,11 +72,19 @@ class Trainer():
             iter_loss += loss.item()
             iter_dice += dice(out, msk)
 
-            tk0.set_postfix(lr=f"{self.optimizer.param_groups[0]['lr']:.4g}",
+            tk1.set_postfix(lr=f"{self.optimizer.param_groups[0]['lr']:.4g}",
                             loss=f"{iter_loss/i:.4g}",
                             dice=f"{iter_dice/i:.4g}")
         
-        return iter_loss / batches, iter_dice / batches
+        iter_loss /= batches
+        iter_dice /= batches
+        save_log(self.log_dir, self.fold, self.nb_splits, 
+                 phase, epoch, iter_loss, iter_dice)
+        
+        torch.cuda.empty_cache()
+        gc.collect()
+
+        return iter_loss, iter_dice
     
 
     def fit(self):
@@ -82,13 +94,19 @@ class Trainer():
         loss_patience = 0
         dice_patience = 0
 
-        for epoch in range(self.epochs):
+        tk0 = tqdm(range(self.epochs), total=self.epochs)
+        for epoch in tk0:
             print(f"Epoch: {epoch+1} / {self.epochs}")
-            train_loss, train_dice = self.iteration('train')
+            train_loss, train_dice = self.iteration(epoch, 'train')
 
             with torch.inference_mode():
-                valid_loss, valid_dice = self.iteration('valid')
+                valid_loss, valid_dice = self.iteration(epoch, 'valid')
 
+            tk0.set_postfix(train_loss=f"{train_loss:.4g}",
+                            train_dice=f"{train_dice:.4g}",
+                            valid_loss=f"{valid_loss:.4g}",
+                            valid_dice=f"{valid_dice:.4g}")
+            
             if valid_loss < best_loss:
                 best_loss = valid_loss
                 loss_patience = 0
@@ -110,5 +128,5 @@ class Trainer():
                 print(f"Model doesn't improved for {self.patience} epochs. Stop training.")
                 break
             print()
-
-        print(f"Done. Best Loss: {best_loss:.4g} | Best Dice: {best_dice:.4g}")
+        clear_output()
+        print(f"{self.fold+1} fold Done. Best Loss: {best_loss:.4g} | Best Dice: {best_dice:.4g}")
