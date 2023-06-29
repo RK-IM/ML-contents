4239549fffbd641bf255ae2bec12ec666df0eb27
RG-Im <94027045+RK-IM@users.noreply.github.com>
2023-06-29 19:00:47 +0900

    Update dataset.py

--- PyTorch/yolo_v1/dataset.py
+++ PyTorch/yolo_v1/dataset.py
@@ -12,14 +12,32 @@ from albumentations.pytorch import ToTensorV2
 import warnings
 warnings.filterwarnings('ignore')
 
-from params import CLASS_IDX
+from params import CLASS_IDX, MEAN, STD
 
 
 class VOCDataset(Dataset):
+    """
+    Pascal VOC dataset
+    """
     def __init__(self, img_path, label_csv,
                  size=448,
                  transform=None,
                  S=7, B=2, C=20):
+        """
+        Constructor.
+
+        Args:
+            img_path (str): Datapath where image file lives.
+            label_csv (str): Csv file path. This file has image id, class, 
+                image size, bounding box informations.
+            size (int): Target image size. Defaults to 448.
+            transform (albumentations transforms): Apply transforms to images.
+                Defaults to None, which apply Resize to `size`, Normalization
+                and change data types to tensor.
+            S (int): Grid size. Defaults to 7.
+            B (int): Number of boxes used for prediction. Defaults to 2.
+            C (int): Classes in Pascal VOC. Defaults to 20.
+        """
         self.img_list = list(Path(img_path).glob('./*jpg'))
         self.label_csv = pd.read_csv(label_csv)
         self.size = size
@@ -34,6 +52,13 @@ class VOCDataset(Dataset):
     
 
     def __getitem__(self, index):
+        """
+        Return one image and label matrix
+
+        Returns:
+            image (torch.Tensor[channels, height, width])
+            label_matrix (torch.Tensor[S, S, C + B*5])
+        """
         img_path = self.img_list[index]
         image, bboxes, class_ids = self._data_gen(img_path)
 
@@ -49,9 +74,11 @@ class VOCDataset(Dataset):
             tfms = A.Compose(
                 [
                     A.Resize(self.size, self.size),
+                    A.Normalize(mean=MEAN, std=STD),
                     ToTensorV2(),
                 ],
-                bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_ids'])
+                bbox_params=A.BboxParams(format='pascal_voc', 
+                                         label_fields=['class_ids'])
             )
             img_bb_cls = tfms(image=image,
                               bboxes=bboxes,
@@ -65,6 +92,18 @@ class VOCDataset(Dataset):
 
     
     def _data_gen(self, img_path):
+        """
+        Load one image. 
+        And return bounding boxes and classes in the loaded image contains.
+        
+        Args:
+            img_path (PosixPath): target image path.
+
+        Returns:
+            image (numpy.array[height, width, channels])
+            bboxes (numpy.array[nb_labels, coordinates(4)])
+            class_ids (numpy.array[nb_labels])
+        """
         img_id = img_path.stem
 
         image = cv2.imread(str(img_path))
@@ -80,6 +119,19 @@ class VOCDataset(Dataset):
     
 
     def _label_convert(self, class_ids, bboxes):
+        """
+        Combine class ids and bounding boxes into one list.
+
+        Args:
+            class_ids (numpy.array): Class ids included in the current image.
+            bboxes (numpy.array): Bounding boxes included in the current image.
+
+        Returns:
+            label_matrix (numpy.array[S, S, C + B*5]): The label matrix
+                has 30 elements in each cell. 20 predictions for the class,
+                2 bounding boxes with 5 predictions which has
+                4 coordinates and probability that an object exists in it.
+        """
         label_matrix = torch.zeros((self.S, self.S, self.C + 5 * self.B))
         for class_id, bbox in zip(class_ids, bboxes):
             xmin, ymin, xmax, ymax = map(lambda x: x/self.size, bbox)
@@ -107,14 +159,26 @@ class VOCDataset(Dataset):
 
 
 def train_tfms(size=448):
+    """
+    Transforms for training images.
+    Apply random scaling and translations with  up to 20%
+    of original image size. And adjust brightness 
+    and saturation up to 50% of values.
+
+    Return:
+        (albumentations transform)
+    """
     return A.Compose(
         [
             A.Resize(size, size),
             A.ShiftScaleRotate(shift_limit=0.2,
                                scale_limit=0.2,
                                rotate_limit=0),
-            A.ColorJitter(saturation=0.5,
+            A.ColorJitter(brightness=0.5,
+                          saturation=0.5,
                           hue=0),
+
+            A.Normalize(mean=MEAN, std=STD),
             ToTensorV2(),
         ],
         bbox_params=A.BboxParams(format='pascal_voc', label_fields=['class_ids'])
@@ -122,9 +186,17 @@ def train_tfms(size=448):
 
 
 def inference_tfms(size=448):
+    """
+    Transforms for test images.
+    Resize and normalize images.
+
+    Return:
+        (albumentations transform)
+    """
     return A.Compose(
         [
             A.Resize(size, size),
+            A.Normalize(mean=MEAN, std=STD),
             ToTensorV2(),
         ]
     )
