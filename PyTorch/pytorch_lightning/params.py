4c9d86a5fc2b62c172d1fe613c4c142020782c06
RG-Im <94027045+RK-IM@users.noreply.github.com>
2023-08-24 00:52:32 +0900

    Delete PyTorch/pytorch_lightning directory

--- PyTorch/pytorch_lightning/params.py
+++
@@ -1,8 +0,0 @@
-import os
-
-DATA_PATH = "../data"
-TRAIN_PATH = os.path.join(DATA_PATH, "training_images")
-TEST_PATH = os.path.join(DATA_PATH, "testing_images")
-TRAIN_CSV = "train_solution_bounding_boxes (1).csv"
-
-ARCHITECTURE = "tf_efficientnetv2_s"
